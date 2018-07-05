c605e22a3600691cb6ed118eedb5ef5ab6d3121a
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-07-05 18:28:10 -0500

    Add test programs

--- message_test.c
+++ message_test.c
@@ -10,32 +10,32 @@ int main(void)
 	int c=0;
 	int b=0;
 	int operations;
-	int pp[2]
+	int pp[2];
 	pipe(&pp[0]);
+	int pid;
 	pid=fork;
 	if (pid>0)
 	{
 		int n;
 		int message;
 
-	while(1){
-		operations = cycles;
-		for (int i = 0; i < operations; ++i)
-		{
-			b=a+0;
-			for (int j = 0; j < mult; ++j)
+		while(1){
+			operations = cycles;
+			for (int i = 0; i < operations; ++i)
 			{
-				c=a+0;
+				b=a+0;
+				for (int j = 0; j < mult*2; ++j)
+				{
+					c=a+0;
+				}
+			}
+			if (1)
+			{
+				n=read(pp[0], &message, sizeof(message));
+				printf("El mensaje es: %d\n", message);
 			}
 		}
-		if (1)
-		{
-			n=read(pp[0], &message, sizeof(message));
-			printf("El mensaje es: %d\n", message);
-		}
-
-	}else if (pid==0)
-	{
+	}else if (pid==0){
 		while (1){
 			int n;
 			int message=0;
