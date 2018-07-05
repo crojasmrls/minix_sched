501fbddc49406adee6dd298e794ed6deaacb6e33
Carlos Rojas Morales <carlosrojo3000@yahoo.com.mx>
2018-07-05 16:55:19 -0500

    Add test programs

--- read_message.c
+++ read_message.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#define cycles 100000
-#define mult   10000
+#define cycles 10000
+#define mult   1000
 
 int main(int argc, char const *argv[])
 {
+	int a=0;	
+	int c=0;
+	int b=0;
 	char message [80];
 	while(1){
-		printf("Ingrese un mensaje:\n");
 		scanf ("%79s",message);  
+		operations = cycles;
+		for (int i = 0; i < operations; ++i)
+		{
+			b=a+0;
+			for (int j = 0; j < mult; ++j)
+			{
+				c=a+0;
+			}
+		}
+		printf("Ingrese un mensaje:\n");
 	 	printf ("El mensaje es: %s\n", message);
 	}
 	return 0;
